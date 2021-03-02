0b82707e3d573072ab46c57b2cb2929dd2d9218b
Sung M. Kim <sungmkim80@gmail.com>
2021-03-02 16:14:38 -0500

    exported all easing buttons

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -11,43 +11,80 @@ let meta = {
   //   easing: { control: 'color' },
   // },
 };
-// let meta = {
-//   title: 'Example/Button',
-//   component: Button,
-//   argTypes: {
-//     backgroundColor: { control: 'color' },
-//   },
-// };
 
 export default meta as Meta;
 
 const Template: Story<ButtonProps> = args => <Button {...args} />;
 
 export const EaseInBack = Template.bind({});
-EaseInBack.args = {
-  easing: 'ease-in-back',
-  children: 'ease-in-back',
+EaseInBack.args = { easing: 'ease-in-back', children: 'ease-in-back' };
+export const EaseInCirc = Template.bind({});
+EaseInCirc.args = { easing: 'ease-in-circ', children: 'ease-in-circ' };
+export const EaseInCubic = Template.bind({});
+EaseInCubic.args = { easing: 'ease-in-cubic', children: 'ease-in-cubic' };
+export const EaseInExpo = Template.bind({});
+EaseInExpo.args = { easing: 'ease-in-expo', children: 'ease-in-expo' };
+export const EaseInOutBack = Template.bind({});
+EaseInOutBack.args = {
+  easing: 'ease-in-out-back',
+  children: 'ease-in-out-back',
 };
-
-// export const Primary = Template.bind({});
-// Primary.args = {
-//   primary: true,
-//   label: 'Button',
-// };
-
-// export const Secondary = Template.bind({});
-// Secondary.args = {
-//   label: 'Button',
-// };
-
-// export const Large = Template.bind({});
-// Large.args = {
-//   size: 'large',
-//   label: 'Button',
-// };
-
-// export const Small = Template.bind({});
-// Small.args = {
-//   size: 'small',
-//   label: 'Button',
-// };
+export const EaseInOutCirc = Template.bind({});
+EaseInOutCirc.args = {
+  easing: 'ease-in-out-circ',
+  children: 'ease-in-out-circ',
+};
+export const EaseInOutCubic = Template.bind({});
+EaseInOutCubic.args = {
+  easing: 'ease-in-out-cubic',
+  children: 'ease-in-out-cubic',
+};
+export const EaseInOutExpo = Template.bind({});
+EaseInOutExpo.args = {
+  easing: 'ease-in-out-expo',
+  children: 'ease-in-out-expo',
+};
+export const EaseInOutQuad = Template.bind({});
+EaseInOutQuad.args = {
+  easing: 'ease-in-out-quad',
+  children: 'ease-in-out-quad',
+};
+export const EaseInOutQuart = Template.bind({});
+EaseInOutQuart.args = {
+  easing: 'ease-in-out-quart',
+  children: 'ease-in-out-quart',
+};
+export const EaseInOutQuint = Template.bind({});
+EaseInOutQuint.args = {
+  easing: 'ease-in-out-quint',
+  children: 'ease-in-out-quint',
+};
+export const EaseInOutSine = Template.bind({});
+EaseInOutSine.args = {
+  easing: 'ease-in-out-sine',
+  children: 'ease-in-out-sine',
+};
+export const EaseInQuad = Template.bind({});
+EaseInQuad.args = { easing: 'ease-in-quad', children: 'ease-in-quad' };
+export const EaseInQuart = Template.bind({});
+EaseInQuart.args = { easing: 'ease-in-quart', children: 'ease-in-quart' };
+export const EaseInQuint = Template.bind({});
+EaseInQuint.args = { easing: 'ease-in-quint', children: 'ease-in-quint' };
+export const EaseInSine = Template.bind({});
+EaseInSine.args = { easing: 'ease-in-sine', children: 'ease-in-sine' };
+export const EaseOutBack = Template.bind({});
+EaseOutBack.args = { easing: 'ease-out-back', children: 'ease-out-back' };
+export const EaseOutCirc = Template.bind({});
+EaseOutCirc.args = { easing: 'ease-out-circ', children: 'ease-out-circ' };
+export const EaseOutCubic = Template.bind({});
+EaseOutCubic.args = { easing: 'ease-out-cubic', children: 'ease-out-cubic' };
+export const EaseOutExpo = Template.bind({});
+EaseOutExpo.args = { easing: 'ease-out-expo', children: 'ease-out-expo' };
+export const EaseOutQuad = Template.bind({});
+EaseOutQuad.args = { easing: 'ease-out-quad', children: 'ease-out-quad' };
+export const EaseOutQuart = Template.bind({});
+EaseOutQuart.args = { easing: 'ease-out-quart', children: 'ease-out-quart' };
+export const EaseOutQuint = Template.bind({});
+EaseOutQuint.args = { easing: 'ease-out-quint', children: 'ease-out-quint' };
+export const EaseOutSine = Template.bind({});
+EaseOutSine.args = { easing: 'ease-out-sine', children: 'ease-out-sine' };
--- stories/Button.tsx
+++ stories/Button.tsx
@@ -1,28 +1,4 @@
 import React from 'react';
-// import './button.css';
-
-// export interface ButtonProps {
-//   /**
-//    * Is this the principal call to action on the page?
-//    */
-//   primary?: boolean;
-//   /**
-//    * What background color to use
-//    */
-//   backgroundColor?: string;
-//   /**
-//    * How large should the button be?
-//    */
-//   size?: 'small' | 'medium' | 'large';
-//   /**
-//    * Button contents
-//    */
-//   label: string;
-//   /**
-//    * Optional click handler
-//    */
-//   onClick?: () => void;
-// }
 
 export const easings = [
   'ease-in-back',
@@ -64,14 +40,7 @@ export interface ButtonProps {
 /**
  * Primary UI component for user interaction
  */
-export const Button: React.FC<ButtonProps> = ({
-  // primary = false,
-  // size = 'medium',
-  // backgroundColor,
-  // label,
-  easing,
-  children,
-}) => {
+export const Button: React.FC<ButtonProps> = ({ easing, children }) => {
   return (
     <button
       type="button"
@@ -80,24 +49,4 @@ export const Button: React.FC<ButtonProps> = ({
       {children}
     </button>
   );
-
-  // const mode = primary
-  //   ? 'storybook-button--primary'
-  //   : 'storybook-button--secondary';
-  // return (
-  //   <button
-  //     type="button"
-  //     className={[
-  //       'text-3xl',
-  //       'text-pink-500',
-  //       'storybook-button',
-  //       `storybook-button--${size}`,
-  //       mode,
-  //     ].join(' ')}
-  //     style={{ backgroundColor }}
-  //     {...props}
-  //   >
-  //     {label}
-  //   </button>
-  // );
 };
